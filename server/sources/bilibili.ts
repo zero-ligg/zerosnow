e35f285fdca1bc11e63a3aa66d6b94f507f550a2
Ou <orongxing@gmail.com>
2024-10-26 21:26:56 +0800

    fix(source): auto get bilibili cookie

--- server/sources/bilibili.ts
+++ server/sources/bilibili.ts
@@ -20,15 +20,11 @@ interface Res {
 
 const hotSearch = defineSource(async () => {
   const url = "https://api.bilibili.com/x/web-interface/wbi/search/square?limit=30"
-  // const cookie = (await $fetch.raw("https://www.bilibili.com/")).headers.getSetCookie()
-  // console.log(cookie)
+  const cookie = (await $fetch.raw("https://api.bilibili.com")).headers.getSetCookie()
   const res: Res = await $fetch(url, {
     headers: {
       "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36",
-      "cookie": [
-        "buvid3=3CDB9CD0-5C2C-708C-60D7-85C480E90C6856761infoc; path=/; expires=Sun, 26 Oct 2025 13:17:36 GMT; domain=.bilibili.com",
-        "b_nut=1729948656; path=/; expires=Sun, 26 Oct 2025 13:17:36 GMT; domain=.bilibili.com",
-      ].join(";"),
+      "cookie": cookie.join(";"),
     },
   })
 
