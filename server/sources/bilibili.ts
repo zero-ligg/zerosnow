04d63a040d8e7b2573134c8ea1859745551db0c1
Ou <orongxing@gmail.com>
2024-10-26 21:15:03 +0800

    fix(source): bili

--- server/sources/bilibili.ts
+++ server/sources/bilibili.ts
@@ -20,11 +20,11 @@ interface Res {
 
 const hotSearch = defineSource(async () => {
   const url = "https://api.bilibili.com/x/web-interface/wbi/search/square?limit=30"
-
+  const cookie = (await $fetch.raw("https://www.bilibili.com/")).headers.getSetCookie()
   const res: Res = await $fetch(url, {
     headers: {
       "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36",
-      // "Cookie": "your_cookie_here"
+      "cookie": cookie.join("; "),
     },
   })
 
