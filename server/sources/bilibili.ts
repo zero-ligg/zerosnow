5f79814e2914572b7f464658913f8e9bae0db07f
Ou <orongxing@gmail.com>
2024-10-26 21:59:23 +0800

    fix(source): bili

--- server/sources/bilibili.ts
+++ server/sources/bilibili.ts
@@ -24,7 +24,7 @@ const hotSearch = defineSource(async () => {
   const res: Res = await $fetch(url, {
     headers: {
       "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36",
-      "cookie": cookie.join(";").match(/buvid3=.+?;/g)?.[0] ?? "",
+      "cookie": cookie.join(";").replace(/.tv/g, ".com"),
       "referer": "https://www.bilibili.com/",
     },
   })
