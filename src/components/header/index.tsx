b690590b9e4b3aff46c17e3b905e642c589278b4
zero-ligg <80372908+zero-ligg@users.noreply.github.com>
2025-02-20 09:19:26 +0800

    Update index.tsx

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -53,9 +53,6 @@ export function Header() {
             </p>
           </span>
         </Link>
-        <a target="_blank" href={`${Homepage}/releases/tag/v${Version}`} className="btn text-sm ml-1 font-mono">
-          {`v${Version}`}
-        </a>
       </span>
       <span className="justify-self-center">
         <span className="hidden md:(inline-block)">
